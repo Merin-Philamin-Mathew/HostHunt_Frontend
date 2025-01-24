5b880e859f38719ec49f4b59c4a2dcbcb51f8448
Merin Mathew <merinphilaminmathew19@gmail.com>
2025-01-24 12:07:10 +0530

    checking bar diagram problem in owner side

--- src/pages/property_owner/PODashboardPage.jsx
+++ src/pages/property_owner/PODashboardPage.jsx
@@ -4,8 +4,8 @@ import React from 'react'
 function PODashboardPage() {
   return (
     <div className="max-w-6xl mx-auto">
-
-    <PODashboard/>
+dashboard
+    {/* <PODashboard/> */}
     </div>
   )
 }
